2ce25b177e1579418750e454bdb35d149d5101e5
AbdalrahmanBashir <s1489740@student.mcckc.edu>
2023-06-28 14:48:13 -0500

    Commit 2: Implement Program class and Main method
    Create the Program class with the Main method.
    Instantiate a CoffeeMaker object with default values.
    Output the initial state of the coffee maker's properties.
    Call the Mimic() method to simulate the brewing process.

---
+++ CoffeeBrewerPro/Program.cs
@@ -0,0 +1,26 @@
+﻿using CoffeeBrewerPro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeBrewerPro
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CoffeeMaker coffee = new("Keurig K-Cafe", 50, true, true);
+
+            System.Console.WriteLine($"Machine brand is: {coffee.GetBrandName()}");
+            System.Console.WriteLine($"Reservoir Capacity is: {coffee.GetCupSizeInOunces()}");
+            System.Console.WriteLine($"Switch is: {coffee.GetIsTurnOnOrOff()}");
+            System.Console.WriteLine($"Basket: {coffee.GetIsInserted()}");
+            coffee.Mimic();
+
+            System.Console.ReadLine();
+
+        }
+    }
+}
