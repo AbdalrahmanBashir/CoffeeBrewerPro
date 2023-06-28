950edc24352761e83d298934fa43f3be7ae3e8e5
AbdalrahmanBashir <s1489740@student.mcckc.edu>
2023-06-28 14:53:05 -0500

    Commit 3: Write unit tests
    Create a new unit test project for the CoffeeMaker class.
    Write unit tests for the Mimic() method to cover different scenarios.

---
+++ CoffeeBrewerPro.Test/CoffeeMakerTests.cs
@@ -0,0 +1,83 @@
+﻿using Xunit;
+
+
+namespace CoffeeBrewerPro.Test
+{
+    public class CoffeeMakerTests
+    {
+        private readonly CoffeeMaker coffeeMaker;
+        private readonly StringWriter outputWriter;
+
+        public CoffeeMakerTests()
+        {
+            coffeeMaker = new CoffeeMaker();
+            outputWriter = new StringWriter();
+            Console.SetOut(outputWriter);
+        }
+
+        [Fact]
+        public void Mimic_ShouldBrewCoffee_WhenSwitchAndBasketAreInCorrectPosition()
+        {
+            // Arrange
+            coffeeMaker.SetIsTurnOnOrOff(true);
+            coffeeMaker.SetIsInserted(true);
+            coffeeMaker.SetCupSizeInOunces(7.00);
+
+            // Act
+            coffeeMaker.Mimic();
+
+            // Assert
+            Assert.Contains("Brewing coffee using: 7 ounces of water.", outputWriter.ToString());
+        }
+
+        [Fact]
+        public void Mimic_ShouldNotBrewCoffee_WhenSwitchIsOff()
+        {
+            // Arrange
+            coffeeMaker.SetIsTurnOnOrOff(false);
+            coffeeMaker.SetIsInserted(true);
+            coffeeMaker.SetCupSizeInOunces(8.0);
+
+            // Act
+            coffeeMaker.Mimic();
+
+            // Assert
+            Assert.Contains("Cannot brew coffee!!! BASKET: True SWITCH: False.", outputWriter.ToString());
+        }
+
+        [Fact]
+        public void Mimic_ShouldNotBrewCoffee_WhenBasketIsNotInserted()
+        {
+            // Arrange
+            coffeeMaker.SetIsTurnOnOrOff(true);
+            coffeeMaker.SetIsInserted(false);
+            coffeeMaker.SetCupSizeInOunces(8.0);
+
+            // Act
+            coffeeMaker.Mimic();
+
+            // Assert
+            Assert.Contains("Cannot brew coffee!!! BASKET: False SWITCH: True.", outputWriter.ToString());
+        }
+
+        [Fact]
+        public void Mimic_ShouldNotBrewCoffee_WhenSwitchAndBasketAreNotInCorrectPosition()
+        {
+            // Arrange
+            coffeeMaker.SetIsTurnOnOrOff(false);
+            coffeeMaker.SetIsInserted(false);
+            coffeeMaker.SetCupSizeInOunces(8.0);
+
+            // Act
+            coffeeMaker.Mimic();
+
+            // Assert
+            Assert.Contains("Cannot brew coffee!!! BASKET: False SWITCH: False.", outputWriter.ToString());
+        }
+        [Fact]
+        public void Dispose()
+        {
+            outputWriter.Dispose();
+        }
+    }
+}
